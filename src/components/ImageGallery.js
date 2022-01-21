173ffba3682b20fb3ed15773e7c9e968fd99ecac
samuel shuman <81707204+kucherjavij@users.noreply.github.com>
2022-01-21 20:52:36 +0200

    final

--- src/components/ImageGallery.js
+++ src/components/ImageGallery.js
@@ -1,44 +1,46 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import ImageGalleryItem from './ImageGalleryItem'
 import {ImageGalleryList} from './ImageGalleryItem.styled'
 import fetchIMG from './API';
 
-class ImageGallery extends Component {
 
-    state ={
-        picture: null,
-        error: null,
-        status: 'idle',
-    }
-
-componentDidUpdate(prevProps, prevState) {
-    const prevName = prevProps.pictureName;
-    const nextName = this.props.pictureName;
-    const prevPage = prevProps.page;
-    const nextPage = this.props.page;
-
-    
-    if (prevName !== nextName) {
-        this.setState({status: 'pending'});
-        fetchIMG(nextName, 1)
-        .then(picture => this.setState({picture: picture.hits, status: 'resolved'}))
-        .catch(error => this.setState({error, status: 'rejected'}) )
+function ImageGallery ({page, pictureName}) {
+const [pictures, setPicture] = useState(null);
+const [error, setError] = useState(null);
+const [status, setStatus] = useState('idle');
+   
+useEffect(() => {
+    if (!pictureName) {
         return
     }
+    
+    setStatus('pending');
+    fetchIMG(pictureName, 1)
+    .then(picture => {
+        setPicture(picture.hits);
+        setStatus('resolved')})
+    .catch(error =>{ 
+        setError(error);
+        setStatus('rejected')} )
+}, [pictureName]);
 
-    if (prevPage !== nextPage) {
-        fetchIMG(nextName, this.props.page)
-        .then(picture => this.setState({picture: [
-            ...prevState.picture,
-            ...picture.hits,
-          ], status: 'resolved'}))
-        .catch(error => this.setState({error, status: 'rejected'}) )}
-return
-}
-
-
-    render() { 
-        const { picture, error , status} = this.state
+useEffect(() => {
+    if (!pictureName) {
+        return
+    }
+    if (page !== 1) {
+        fetchIMG(pictureName, page)
+    .then(picture =>{ 
+        setPicture(
+       [ ...pictures,
+        ...picture.hits,]
+      );
+       setStatus('resolved')})
+    .catch(error => {
+        setError(error);
+        setStatus('rejected')})
+    }
+}, [page]);
       
 if (status === 'idle') {
     return <div>Write something...</div>
@@ -55,15 +57,77 @@ if (status === 'idle') {
           if (status === 'resolved') {
               return <ImageGalleryList>
                   
-              {picture.map(({webformatURL, id, largeImageURL})=><ImageGalleryItem URL={webformatURL} LargeUrl={largeImageURL} key={id}>    
+              {pictures.map(({webformatURL, id, largeImageURL})=><ImageGalleryItem URL={webformatURL} LargeUrl={largeImageURL} key={id}>    
               
               </ImageGalleryItem>)}
               </ImageGalleryList>
               
           }
 
-    }
+    
 }
+
+// class ImageGallery extends Component {
+
+//     state ={
+//         picture: null,
+//         error: null,
+//         status: 'idle',
+//     }
+
+// componentDidUpdate(prevProps, prevState) {
+//     const prevName = prevProps.pictureName;
+//     const nextName = this.props.pictureName;
+//     const prevPage = prevProps.page;
+//     const nextPage = this.props.page;
+
+    
+//     if (prevName !== nextName) {
+//         this.setState({status: 'pending'});
+//         fetchIMG(nextName, 1)
+//         .then(picture => this.setState({picture: picture.hits, status: 'resolved'}))
+//         .catch(error => this.setState({error, status: 'rejected'}) )
+//         return
+//     }
+
+//     if (prevPage !== nextPage) {
+//         fetchIMG(nextName, this.props.page)
+//         .then(picture => this.setState({picture: [
+//             ...prevState.picture,
+//             ...picture.hits,
+//           ], status: 'resolved'}))
+//         .catch(error => this.setState({error, status: 'rejected'}) )}
+// return
+// }
+
+
+//     render() { 
+//         const { picture, error , status} = this.state
+      
+// if (status === 'idle') {
+//     return <div>Write something...</div>
+// }
+
+//         if (status === 'pending') {
+//             return  <div>Loading...</div>
+//           }
+
+//           if (status === 'rejected') {
+//               return <h2>{error.message}</h2>
+//           }
+
+//           if (status === 'resolved') {
+//               return <ImageGalleryList>
+                  
+//               {picture.map(({webformatURL, id, largeImageURL})=><ImageGalleryItem URL={webformatURL} LargeUrl={largeImageURL} key={id}>    
+              
+//               </ImageGalleryItem>)}
+//               </ImageGalleryList>
+              
+//           }
+
+//     }
+// }
  
 export default ImageGallery;
 
